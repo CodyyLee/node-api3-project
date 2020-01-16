ee53de9d5e4ba1696d742fa59bfe7fc0c5cfe25d
Cody Garrett <cody.l.garrett22@gmail.com>
2020-01-16 12:30:19 -0700

    added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // code away!
 const server = require("./server.js");
 
-server.listen(4000, () => {
+server.listen(process.env.PORT || 4000, () => {
     console.log(`\n ** Server listening on port:4000 **\n`)
 })
 
